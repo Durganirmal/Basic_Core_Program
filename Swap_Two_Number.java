bd74bd138bcc72c407d9e17d122889424d45cfda
Durga Nirmal <durganirmal123@gmail.com>
2021-11-07 11:46:39 +0530

    Added :Swapping the number

---
+++ Swap_Two_Number.java
@@ -0,0 +1,21 @@
+package com.Basic_Core_Program;
+
+import java.util.Scanner;
+public class Swap_Two_Number {
+
+	public static void main(String[] args) {
+        Scanner y =new Scanner(System.in);
+        System.out.println("Enter the two number to swap them ");
+        System.out.println("Enter the first number:");
+        int num1 =y.nextInt();
+        System.out.println("Enter the second number:");
+        int num2 =y.nextInt();
+        int temp;
+        temp=num1;
+        num1=num2;
+        num2=temp;
+                System.out.println("After swapping the first number is "+num1);
+                System.out.println("After swapping the second number is "+num2);
+	}
+
+}
